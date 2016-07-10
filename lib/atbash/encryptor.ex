192c2e9d1c0fa01a43ee4b44c9fd901b2cdf6fe1
Jake Worth <jake@jakeworth.com>
2016-07-10 16:30:58 -0500

    Support lower case letters
    Close #1

--- lib/atbash/encryptor.ex
+++ lib/atbash/encryptor.ex
@@ -11,6 +11,9 @@ defmodule Atbash.Encryptor do
       iex> Atbash.Encryptor.encrypt("ATTACK AT DAWN!")
       "ZGGZXP ZG WZDM!"
 
+      iex> Atbash.Encryptor.encrypt("attack at dawn")
+      "zggzxp zg wzdm"
+
       iex> Atbash.Encryptor.encrypt("")
       ""
   """
@@ -26,7 +29,9 @@ defmodule Atbash.Encryptor do
     atbash_map = [ A: "Z", B: "Y", C: "X", D: "W", E: "V", F: "U", G: "T", H:
       "S", I: "R", J: "Q", K: "P", L: "O", M: "N", N: "M", O: "L", P: "K", Q:
       "J", R: "I", S: "H", T: "G", U: "F", V: "E", W: "D", X: "C", Y: "B", Z:
-      "A" ]
+      "A", a: "z", b: "y", c: "x", d: "w", e: "v", f: "u", g: "t", h: "s", i:
+      "r", j: "q", k: "p", l: "o", m: "n", n: "m", o: "l", p: "k", q: "j", r:
+      "i", s: "h", t: "g", u: "f", v: "e", w: "d", x: "c", y: "b", z: "a" ]
 
     encoded = atbash_map[String.to_atom(h)] || h
 
