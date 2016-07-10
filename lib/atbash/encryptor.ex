1342483e6fdbaa34225f644f22881217497bf4f6
Jake Worth <jake@jakeworth.com>
2016-07-10 16:25:00 -0500

    Remove trailing comma

--- lib/atbash/encryptor.ex
+++ lib/atbash/encryptor.ex
@@ -26,7 +26,7 @@ defmodule Atbash.Encryptor do
     atbash_map = [ A: "Z", B: "Y", C: "X", D: "W", E: "V", F: "U", G: "T", H:
       "S", I: "R", J: "Q", K: "P", L: "O", M: "N", N: "M", O: "L", P: "K", Q:
       "J", R: "I", S: "H", T: "G", U: "F", V: "E", W: "D", X: "C", Y: "B", Z:
-      "A", ]
+      "A" ]
 
     encoded = atbash_map[String.to_atom(h)] || " "
 
